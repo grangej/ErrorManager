e92cc560fbc0211a0191f3f56e7a84570fd59da3
John Grange <john.grange@ymedialabs.com>
2015-01-13 09:51:58 -0800

    - Add project files
    Signed-off-by: John Grange <john.grange@ymedialabs.com>

---
+++ ErrorManager/ErrorManager.h
@@ -0,0 +1,19 @@
+//
+//  ErrorManager.h
+//  ErrorManager
+//
+//  Created by John Grange on 1/13/15.
+//  Copyright (c) 2015 Y Media Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ErrorManager.
+FOUNDATION_EXPORT double ErrorManagerVersionNumber;
+
+//! Project version string for ErrorManager.
+FOUNDATION_EXPORT const unsigned char ErrorManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ErrorManager/PublicHeader.h>
+
+
